df0547212cff79ddb9e0196ab60064c09d9d218f
Ollie-Judge <ollie.judge@hotmail.com>
2022-01-19 20:24:00 +0000

    added quantitirs to the shopping cart

--- bag/contexts.py
+++ bag/contexts.py
@@ -27,7 +27,7 @@ def bag_contents(request):
                 product_count += quantity
                 bag_items.append({
                     'item': item_id,
-                    'quantity': item_data,
+                    'quantity': quantity,
                     'product': product,
                     'size': size,
                 })
