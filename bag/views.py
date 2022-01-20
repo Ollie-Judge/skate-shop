bf80f95820cc65388a5ade780139b0aa526c5159
Ollie-Judge <ollie.judge@hotmail.com>
2022-01-20 13:31:27 +0000

    added toassts

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from products.models import Product
 
 
 def view_bag(request):
@@ -9,6 +12,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ adds specified number of selected products to the shopping cart """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -29,6 +33,7 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your cart')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
