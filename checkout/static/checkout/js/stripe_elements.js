e82be1543aec09e0d549efcb89b34eb8ec8c146f
Ollie-Judge <ollie.judge@hotmail.com>
2022-01-22 16:02:27 +0000

    added a loading overlay

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -59,6 +59,8 @@ form.addEventListener('submit', function(ev) {
                 <span>${result.error.message}</span>
             `;
              $(errorDiv).html(html);
+             $('#payment-form').fadeToggle(100);
+             $('#loading-overlay').fadeToggle(100);
              card.update({ 'disabled': false});
              $('#submit-button').attr('disabled', false);
 
