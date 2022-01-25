4a5143712fb13724bd36a59e18d685b5f6d4c779
Ollie-Judge <ollie.judge@hotmail.com>
2022-01-25 17:42:48 +0000

    cleaned up the spelling errors, indentaion errors and type-os

--- profiles/views.py
+++ profiles/views.py
@@ -17,15 +17,19 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Your profile has been successfully updated')
-
-    form = UserProfileForm(instance=profile)
-    orders= profile.orders.all()
+        else:	
+            messages.error(request,	
+                           ('Update failed. Please ensure '	
+                            'the form is valid.'))	
+    else:	
+        form = UserProfileForm(instance=profile)	
+    orders = profile.orders.all()
 
     template = 'profiles/profile.html'
     context = {
         'form': form,
-        'ordeers': orders,
-        'on_profile_page': True,
+        'orders': orders,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
@@ -38,7 +42,7 @@ def order_history(request, order_number):
         'A confirmation was sent on the date of this order.'
     ))
 
-    template = 'checkout/checkout_successs/html'
+    template = 'checkout/checkout_successs.html'
     context = {
         'order': order,
         'from_profile': True,
