1e9f9daa5810e0075ef68bacdc21f9d990aee2ba
Ollie-Judge <ollie.judge@hotmail.com>
2022-01-26 18:10:40 +0000

    added an add product template and url

--- products/forms.py
+++ products/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Product, Category
 
 
-class ProductForm(forms.MOdelForm):
+class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
