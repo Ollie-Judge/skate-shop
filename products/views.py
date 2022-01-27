0c52c3cf1226c7bbee57b7b7977ccefcb4efcfb5
Ollie-Judge <ollie.judge@hotmail.com>
2022-01-27 17:11:07 +0000

    added delete functionality

--- products/views.py
+++ products/views.py
@@ -98,7 +98,7 @@ def edit_product(request, product_id):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'You have successfully updated teh product')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
@@ -114,3 +114,14 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """
+    deletes a product from the store
+    """
+
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product has been deleted!')
+    return redirect(reverse('products'))
