e597d2b8d330a5855100c133930f0495f4dc6201
Ollie-Judge <ollie.judge@hotmail.com>
2022-01-28 17:31:48 +0000

    began construction on the about page by connecting the urls.py and the views.py up and setting a basis for the template

---
+++ about/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def about(request):
+    """
+    renders the about page
+    """
+
+    return render(request, 'about/about.html')
