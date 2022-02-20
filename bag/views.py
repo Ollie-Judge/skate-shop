bbae448a1f546d4e96770bbc530d967974039bc0
Ollie-Judge <ollie.judge@hotmail.com>
2022-02-20 16:43:11 +0000

    fixed add product error, 3 image elements when 2 are needed and the 3rd is outside of the if statement causing the app to break

--- bag/views.py
+++ bag/views.py
@@ -40,6 +40,7 @@ def add_to_bag(request, item_id):
             messages.success(request, f'Added {product.name} to your cart')
 
     request.session['bag'] = bag
+    print(request.session['bag'])
     return redirect(redirect_url)
 
 
