89253f7ab65da1b6cbf167f0ce956e443acdd3df
Ollie-Judge <ollie.judge@hotmail.com>
2022-02-21 19:29:54 +0000

    connected up the contact us page

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def contact(request):
+    """
+    oversees the contact us page form
+    """
+
+    return render(request, 'contact/contact.html')
