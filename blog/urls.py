d331f00cc4c2fb50f15bab09f2188fc68cfa379a
Ollie-Judge <ollie.judge@hotmail.com>
2022-02-26 13:54:02 +0000

    fixed the read more button not loading up the post detail page

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='blog'),
-    path('<slug:slug>/', views.PostList.as_view(), name='post_detail'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
