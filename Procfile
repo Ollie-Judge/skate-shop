c3ac9836885a36bafa0936edbd0cfbce555f1fb4
Ollie-Judge <ollie.judge@hotmail.com>
2022-02-27 11:36:28 +0000

    second part of deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn skate-shop.wsgi:application
--- skate_shop/settings.py
+++ skate_shop/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['skate-shop123.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -124,13 +124,17 @@ WSGI_APPLICATION = 'skate_shop.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-   }
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
