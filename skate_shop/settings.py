6207be57caf1013e117aeac43b5d3d40b4aafc8b
Ollie-Judge <ollie.judge@hotmail.com>
2022-02-27 10:31:44 +0000

    first part of deployment

--- skate_shop/settings.py
+++ skate_shop/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 if os.path.exists("env.py"):
   import env 
@@ -126,11 +127,10 @@ WSGI_APPLICATION = 'skate_shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
+   }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
