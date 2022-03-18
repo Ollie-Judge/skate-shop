6f6babe8e021ec991394f8f35eed0a2d30b304fe
Ollie-Judge <ollie.judge@hotmail.com>
2022-03-18 13:06:54 +0000

    reverted to base_dir

--- skate_shop/settings.py
+++ skate_shop/settings.py
@@ -188,9 +188,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [
-    os.path.join(SKATE_SHOP, 'static')
+    os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(SKATE_SHOP, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_URL = '/media/'
