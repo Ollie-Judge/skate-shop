4cc431d3afadc29d068a4d5596090e408a9b78d7
Ollie-Judge <ollie.judge@hotmail.com>
2022-03-18 13:13:23 +0000

    change dsquare parenthesis to rounded

--- skate_shop/settings.py
+++ skate_shop/settings.py
@@ -187,9 +187,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static')
-]
+)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
